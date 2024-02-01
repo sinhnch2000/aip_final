1e1a9e308f22841c0d12ad28490d103f1f4e5887
This PC <sinhnch2001@gmail.com>
2024-02-01 17:45:03 +0700

    bla

--- src/old_train/scripts/run_training_int.sh
+++ src/old_train/scripts/run_training_int.sh
@@ -8,7 +8,7 @@ CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file /kaggle/working/aip_fin
         --output_dir "./output/GradInt/0102"  \
         --train_files  "/kaggle/input/data-gradint/GradINT/FUSEDCHAT/train.json" "/kaggle/input/data-gradint/GradINT/SGD/train.json"\
         --val_files  "/kaggle/input/data-gradint/GradINT/FUSEDCHAT/val.json" "/kaggle/input/data-gradint/GradINT/SGD/train.json"\
-        --resume_from_checkpoint "/kaggle/input/ckpt-gradint/epoch_1" \
+        --resume_from_checkpoint "/kaggle/input/ckpt-gradint/epoch_0" \
         --batch_size  8 \
         --num_beams  4 \
         --weight_decay  0.3 \
